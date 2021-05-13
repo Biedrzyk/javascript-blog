83d87036430be827d97f357a5997f6eab6d31aca
Łukasz Biedrzycki <lukasz.biedrzycki@gmail.com>
2021-05-13 21:40:44 +0200

    Add new loop for removing class active

--- js/script.js
+++ js/script.js
@@ -5,15 +5,26 @@
     console.log('links:', links);
   });*/
 
-  const titleClickHandler = function(){
-    console.log('Link was clicked!');
-    console.log(event);
+const titleClickHandler = function () {
+  console.log('Link was clicked!');
+  console.log(event);
 
   /* remove class 'active' from all article links  */
 
+  const activeLinks = document.querySelectorAll('.titles a.active');
+
+  for (let activeLink of activeLinks) {
+    activeLink.classList.remove('active');
+  }
   /* add class 'active' to the clicked link */
 
   /* remove class 'active' from all articles */
+
+  const activeArticles = document.querySelectorAll('.posts article.active');
+
+  for (let activeArticle of activeArticles) {
+    activeArticle.classList.remove('active');
+  }
 
   /* get 'href' attribute from the clicked link */
 
@@ -21,10 +32,10 @@
 
   /* add class 'active' to the correct article */
 
-  }
-  
-  const links = document.querySelectorAll('.titles a');
-  
-  for(let link of links){
-    link.addEventListener('click', titleClickHandler);
-  }
+}
+
+const links = document.querySelectorAll('.titles a');
+
+for (let link of links) {
+  link.addEventListener('click', titleClickHandler);
+}
