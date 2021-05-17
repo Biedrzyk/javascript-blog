8127d39ae8f969cc1cbf8283962706f5f3ead8cc
Łukasz Biedrzycki <lukasz.biedrzycki@gmail.com>
2021-05-17 20:58:22 +0200

    Add searching for id and title of link options

--- js/script.js
+++ js/script.js
@@ -54,30 +54,41 @@ for (let link of links) {
   link.addEventListener('click', titleClickHandler);
 }
 
-const optArticleSelector = '.post',
-  optTitleSelector = '.post-title',
-  optTitleListSelector = '.titles';
+    const optArticleSelector = '.post',
+    optTitleSelector = '.post-title',
+    optTitleListSelector = '.titles';
 
-function generateTitleLinks(){
+function generateTitleLinks() {
 
-  /* remove contents of titleList */
+    /* remove contents of titleList */
 
-  const titleList = document.querySelector(optTitleListSelector);
-  titleList.innerHTML = '';
-  
+    const titleList = document.querySelector(optTitleListSelector);
+    titleList.innerHTML = '';
 
-  /* for each article */
+
+    /* for each article */
+
+    const articles = document.querySelectorAll(optArticleSelector);
+
+  for (let article of articles) {
 
     /* get the article id */
 
+    const articleId = article.getAttribute('id');
+
     /* find the title element */
+
+    const articleTitle = article.querySelector(optTitleSelector).innerHTML;
 
     /* get the title from the title element */
 
+    const linkHTML = '<li><a href="#' + articleId + '"><span>' + articleTitle + '</span></a></li>';
+    console.log(linkHTML);
+    
     /* create HTML of the link */
 
     /* insert link into titleList */
-
+  }
 }
 
 generateTitleLinks();
